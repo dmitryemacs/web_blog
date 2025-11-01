f5f0a87b021bca1c3384afdf010337084121e92f
dmitryemacs <dimadimof81@gmail.com>
2025-11-01 12:55:06 -0700

    update procjet files and connect postgresql

--- config.py
+++ config.py
@@ -5,3 +5,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'postgresql://dima:zxc011@localhost/blogdb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    UPLOAD_FOLDER = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static/uploads')
+
+    MAX_CONTENT_LENGTH = 50 * 1024 * 1024
