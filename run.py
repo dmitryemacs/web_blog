9d9bf56dc79ea62f129ec32f3864be2886a4f80c
dmitryemacs <dimadimof81@gmail.com>
2025-12-09 23:28:38 +0300

    start point

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 from app import create_app, db
+import os
 
 app = create_app()
 
+# Создаем папку для загрузок при запуске
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
