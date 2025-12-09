3f70b193ceaba7afe85c7c28002b3306e5a4d5d1
dmitryemacs <dimadimof81@gmail.com>
2025-12-09 23:26:28 +0300

    update for dc

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'mysecretkey'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-in-production'
+    
+    # Используем переменную окружения из Docker Compose
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://dima:zxc011@localhost/blogdb'
+        'postgresql://blog_user:blog_password@localhost:5432/blogdb'
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     UPLOAD_FOLDER = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static/uploads')
